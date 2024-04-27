b8fa3e39f158c74ec41c55bf3737eba262e0b786
gashasusev <gavlogin3@gmail.com>
2024-04-27 14:25:53 +0300

    reg form

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,18 +1,17 @@
-
-
+import React, { useState } from 'react';
+import Form_st from "./components/Form_st"
 function App() {
-
   const [showModal, setShowModal] = useState(false);
 
   return (
     <div>
-      <button onClick={() => setShowModal(true)}>Показать модальное окно</button>
-      {showModal && <Modal onClose={() => setShowModal(false)}>
-        <h2>Содержание модального окна</h2>
-        <p>Текст...</p>
-      </Modal>}
+      <button onClick={() => setShowModal(true)}>Открыть форму</button>
+      <Form_st isOpen={showModal} onClose={() => setShowModal(false)} />
+
+      <button onClick={() => setShowModal(true)}>Открыть форму</button>
+      <Form_st isOpen={showModal} onClose={() => setShowModal(false)} />
     </div>
   );
 }
 
-export default App
+export default App;
---
+++ front/src/components/Form_st.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+
+
+function Form_st({ isOpen, onClose }) {
+  const [formData, setFormData] = useState({
+    name: '',
+    surname: '',
+    gender: '',
+  });
+
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+    onClose();
+  };
+
+  if (!isOpen) return null;
+
+  return (
+    <div className="modal" onClick={onClose}>
+      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+        <h2>Введите данные</h2>
+        <form onSubmit={handleSubmit}>
+
+          <label htmlFor="name">ФИО:</label>
+          <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required />
+
+          <label htmlFor="name">Телеграм:</label>
+          <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required />
+
+          <label htmlFor="name">Почта:</label>
+          <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} required />
+
+          <label htmlFor="gender">Каста:</label>
+      <select id="gender" name="gender" value={formData.gender} onChange={handleChange} required>
+        <option value="">Выберите свое направление</option>
+        <option value="male">ML</option>
+        <option value="back">Backend</option>
+        <option value="front">Frontend</option>
+        <option value="Сети и системное администрирование">Сети и системное администрирование</option>
+        <option value="Алгоритмы и структуры данных">Алгоритмы и структуры данных</option>
+      </select>
+          <button type="submit">Отправить</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Form_st
